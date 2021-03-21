9716df06109fe54631da9f43447b4d59b1c46e6e
Nicolasd <nicolas@thelucky.app>
2021-03-21 01:03:58 -0300

    Add part 3 for class 3

--- app/src/main/java/com/example/walletbonustrack/MainActivity.java
+++ app/src/main/java/com/example/walletbonustrack/MainActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText etCvv;
     private EditText etNombre;
     private Button btnAgregar;
+    private CheckBox checkBoxTerms;
+    private TextView textViewTerms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setupUI();
+        setupCheckBoxListener();
     }
 
     private void setupUI() {
@@ -31,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
         etCvv = findViewById(R.id.editTextCvv);
         etNombre = findViewById(R.id.editTextTextPersonName);
         btnAgregar = findViewById(R.id.btnAddCard);
+        checkBoxTerms = findViewById(R.id.checkBoxTerms);
+        textViewTerms = findViewById(R.id.textViewTerms);
 
         btnAgregar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +50,27 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     Toast.makeText(MainActivity.this, "Datos inválidos", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+        textViewTerms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lanzarTermsAndConditionActivity();
+            }
+        });
+    }
+
+    private void lanzarTermsAndConditionActivity() {
+        Intent intent = new Intent(this, TermsAndConditionsActivity.class);
+        startActivity(intent);
+    }
+
+    private void setupCheckBoxListener() {
+        checkBoxTerms.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                btnAgregar.setEnabled(isChecked);
             }
         });
     }
